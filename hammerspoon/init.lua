2b05210c805f311c6eb1f42c75f64a157afe3f1a
LugeK1t <263635591@qq.com>
2025-03-13 11:47:22 +0800

    style: format hammerspoon/init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,24 +1,27 @@
 -- general func
 -- send notify
 function sendNotify(content, title)
-  if title == nil then
-    title = "Hammerspoon"
-  end
-  hs.notify.new({title=title, informativeText=content}):send()
+    if title == nil then
+        title = "Hammerspoon"
+    end
+    hs.notify.new({title=title, informativeText=content}):send()
 end
+
+-- alert
+alert = hs.alert.show
 
 
 -- auto reload when init.lua is changed
 function reloadConfig(files)
-  doReload = true
-  for _, file in pairs(files) do
-      if file:sub(-4) == ".lua" then
-        doReload = true
-      end
-  end
-  if doReload then
-    hs.reload()
-  end
+    doReload = true
+    for _, file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
 end
 configReloadWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/dotfiles/hammerspoon/", reloadConfig)
 configReloadWatcher:start()
@@ -32,45 +35,45 @@ local operationCache = {}
 
 -- resize func
 function moveWindow(direction)
-  local win = hs.window.focusedWindow()
-  local position
-  local id = win:id()
+    local win = hs.window.focusedWindow()
+    local position
+    local id = win:id()
 
-  if frameCache[id] then
-    if operationCache[id] == direction then
-      win:setFrame(frameCache[id])
-      frameCache[id] = nil
-      operationCache[id] = nil
-      return
+    if frameCache[id] then
+        if operationCache[id] == direction then
+            win:setFrame(frameCache[id])
+            frameCache[id] = nil
+            operationCache[id] = nil
+            return
+        else
+            operationCache[id] = direction
+        end
     else
-      operationCache[id] = direction
+        frameCache[id] = win:frame()
+        operationCache[id] = direction
     end
-  else
-    frameCache[id] = win:frame()
-    operationCache[id] = direction
-  end
 
-  if direction == "left" then
-    position = {0, 0, 0.5, 1}
-  elseif direction == "right" then
-    position = {0.5, 0, 0.5, 1}
-  elseif direction == "up" then
-    position = {0, 0, 1, 0.5}
-  elseif direction == "down" then
-    position = {0, 0.5, 1, 0.5}
-  elseif direction == "up_left" then
-    position = {0, 0, 0.5, 0.5}
-  elseif direction == "up_right" then
-    position = {0.5, 0, 0.5, 0.5}
-  elseif direction == "down_left" then
-    position = {0, 0.5, 0.5, 0.5}
-  elseif direction == "down_right" then
-    position = {0.5, 0.5, 0.5, 0.5}
-  elseif direction == "max" then
-    position = {0, 0, 1, 1}
-  end
+    if direction == "left" then
+        position = {0, 0, 0.5, 1}
+    elseif direction == "right" then
+        position = {0.5, 0, 0.5, 1}
+    elseif direction == "up" then
+        position = {0, 0, 1, 0.5}
+    elseif direction == "down" then
+        position = {0, 0.5, 1, 0.5}
+    elseif direction == "up_left" then
+        position = {0, 0, 0.5, 0.5}
+    elseif direction == "up_right" then
+        position = {0.5, 0, 0.5, 0.5}
+    elseif direction == "down_left" then
+        position = {0, 0.5, 0.5, 0.5}
+    elseif direction == "down_right" then
+        position = {0.5, 0.5, 0.5, 0.5}
+    elseif direction == "max" then
+        position = {0, 0, 1, 1}
+    end
 
-  win:move(position)
+    win:move(position)
 end
 
 hs.hotkey.bind({"ctrl", "shift"}, "Left", hs.fnutils.partial(moveWindow, "left"))
@@ -85,17 +88,16 @@ hs.hotkey.bind({"ctrl", "shift"}, "\\", hs.fnutils.partial(moveWindow, "max"))
 
 -- auto change input method
 local appLanguage = {
-  ["/Applications/Raycast.app"] = "English",
-  ["/Applications/iTerm.app"] = "English",
-  ["/Applications/Android Studio.app"] = "English",
-  ["/Applications/Visual Studio Code.app"] = "English",
-  ["/Applications/Google Chrome.app"] = "English",
-  ["/Applications/GoLand.app"] = "English",
-  ["/Applications/PyCharm Community.app"] = "English",
-  ["/Applications/RustRover.app"] = "English",
-  ["/Applications/Tabby.app"] = "English",
-  ["/Applications/Lark.app"] = "Chinese",
-  ["/Applications/WeChat.app"] = "Chinese",
+    ["Raycast"] = "English",
+    ["iTerm"] = "English",
+    ["Android Studio"] = "English",
+    ["Visual Studio Code"] = "English",
+    ["Google Chrome"] = "English",
+    ["GoLand"] = "English",
+    ["PyCharm Community"] = "English",
+    ["RustRover"] = "English",
+    ["Ghostty"] = "English",
+    ["飞书"] = "Chinese",
 }
 
 local languageIM = {
@@ -103,29 +105,28 @@ local languageIM = {
     ["Chinese"] = "com.apple.inputmethod.SCIM.ITABC"
 }
 
-function changeCurrentInput()
-  local focusedAppPath = hs.window.focusedWindow():application():path()
-  local currentSourceID = hs.keycodes.currentSourceID()
+function changeCurrentInput(appName)
+    local currentSourceID = hs.keycodes.currentSourceID()
 
-  local language = appLanguage[focusedAppPath]
-  if language == nil then
-      return
-  end
+    local language = appLanguage[appName]
+    if language == nil then
+        return
+    end
 
-  local sourceID = languageIM[language]
-  if sourceID == nil then
-      return
-  end
+    local sourceID = languageIM[language]
+    if sourceID == nil then
+        return
+    end
 
-  if currentSourceID ~= sourceID then
-      hs.keycodes.currentSourceID(sourceID)
-  end
+    if currentSourceID ~= sourceID then
+        hs.keycodes.currentSourceID(sourceID)
+    end
 end
 
 function appLanguageWatcherFunc(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.activated) then
-    changeCurrentInput()
-  end
+    if (eventType == hs.application.watcher.activated) then
+        changeCurrentInput(appName)
+    end
 end
 
 appLanguageWatcher = hs.application.watcher.new(appLanguageWatcherFunc)
@@ -139,5 +140,11 @@ hs.hotkey.bind({"option"}, "z", function()
         return
     end
     hs.application.launchOrFocus("Ghostty.app")
+end)
+
+-- test
+hs.hotkey.bind({"cmd", "ctrl"}, "t", function()
+    local app = hs.window.focusedWindow():application():name()
+    alert(app)
 end)
 
