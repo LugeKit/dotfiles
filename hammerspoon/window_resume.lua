51d608bb9dd399efc7c285b7360afd9ff29d676e
LugeK1t <263635591@qq.com>
2026-01-04 11:22:05 +0800

    chore: optimize window restoration delay
    1. Reduce screen watcher delay from 2s to 1s in window_resume.lua
    2. Reduce system wake watcher delay from 3s to 1s in window_resume.lua

--- hammerspoon/window_resume.lua
+++ hammerspoon/window_resume.lua
@@ -60,11 +60,11 @@ end
 hs.hotkey.bind({ "cmd", "shift" }, "S", saveWindowLayout)
 hs.hotkey.bind({ "cmd", "shift" }, "R", restoreWindowLayout)
 hs.screen.watcher.new(function()
-    hs.timer.doAfter(2, restoreWindowLayout)
+    hs.timer.doAfter(1, restoreWindowLayout)
 end):start()
 hs.caffeinate.watcher.new(function(eventType)
     if eventType == hs.caffeinate.watcher.systemDidWake then
-        hs.timer.doAfter(3, restoreWindowLayout)
+        hs.timer.doAfter(1, restoreWindowLayout)
     end
 end):start()
 
